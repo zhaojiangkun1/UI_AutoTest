077c1823ca9e6697594a870332bfb12d3088bb22
unknown <agfjiang@126.com>
2019-07-09 18:07:30 +0800

    元类

--- part_seven/my_metaclass.py
+++ part_seven/my_metaclass.py
@@ -1,21 +1,27 @@
 """
 首先有一个类
 """
+
+
 class Person():
     pass
+
 
 # 可以打印一个类
 print(Person)
 p = Person
 print(p)
-p.name  = "张三"
+p.name = "张三"
 print(p.name)
+
 
 # 类可以当作一个参数传递
 def func(p):
     print(p.name)
 
+
 func(p)
+
 
 # 动态的创建一个类
 def get_class(name):
@@ -30,6 +36,75 @@ def get_class(name):
                 print("cat run")
         return Cat
 
+
 c = get_class("Dog")
 print(c)
 c.run(c)
+c = get_class("c")
+print(c)
+c.run(c)
+
+# type
+print(type(1))
+print(type("aaa"))
+
+
+# 使用type来动态的构造一个类
+print(type(c))
+
+
+Person2 = type('Person2',(),{})
+print(Person2)
+p2 = Person2()
+print(p2)
+
+
+# 增加属性
+class Person3():
+    name = "张三"
+
+
+Person3 = type('Person3', (),  {'name':'李四'})
+print(Person3.name)
+
+# 继承
+Person4 = type('Person4', (Person3,), {})
+print(Person4.name)
+
+
+# 增加方法
+def person4_run(self):
+    print("person4 run")
+
+
+Person4 = type('Person4', (Person3,), {'person4_run': person4_run})
+p = Person4()
+p.person4_run()
+
+# 元类
+# 类创造实例
+# 元类是用来创造类
+
+
+class MyMetaClass(type):
+
+    def __new__(cls, name, bases, attrs):
+        print("元类new方法运行")
+        print(name)
+        print(bases)
+        attrs['add'] = lambda self, value:value+value
+        print(attrs)
+        # 下边这一行调用type方法来动态得构建
+        return type.__new__(cls, name, bases, attrs)
+
+
+class MyClass(Person4, metaclass=MyMetaClass):
+    # 上边的metaclass=MyMetaClass 就指定了使用MyMetaClass来定制类
+    # 就是通过type.__new__来创建的
+    pass
+
+
+my_class = MyClass()
+r = my_class.add(2)
+print(r)
+
